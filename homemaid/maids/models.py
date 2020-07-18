03e54700320e5c4fb6cdb06396c52cdc646ddf93
isaman sangbamrung <towlovelove@gmail.com>
2020-07-18 14:10:11 +0700

    update models and test

--- homemaid/maids/models.py
+++ homemaid/maids/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Maid(models.Model):
+    name = models.CharField(max_length=300)
+    birtdate = models.DateField()
+    description = models.TextField()
+    certificate = models.TextField()
+    salary = models.IntegerField()
--- homemaid/maids/tests.py
+++ homemaid/maids/tests.py
@@ -1,3 +1,29 @@
+from datetime import date
 from django.test import TestCase
+from .models import Maid
+
 
 # Create your tests here.
+class TestMaid(TestCase):
+    def test_model_should_have_name_defined_fields(self):
+        # Given
+        Maid.objects.create(
+            name="BB" ,
+            birtdate=date(1998,4,29),
+            description="Super mad of the Year",
+            certificate="Best Maid 2020",
+            salary=3000
+        )
+
+        # When
+        maid = Maid.objects.last()
+
+        #Then
+        self.assertEqual(maid.name , "BB")
+        self.assertEqual(maid.birtdate , date(1998,4,29))
+        self.assertEqual(maid.description , "Super mad of the Year")
+        self.assertEqual(maid.certificate , "Best Maid 2020")
+        self.assertEqual(maid.salary , 3000)
+
+
+        
