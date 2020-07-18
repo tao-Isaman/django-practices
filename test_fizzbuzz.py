1d3f315a40f134173e3df426710197105c735882
isaman sangbamrung <towlovelove@gmail.com>
2020-07-18 10:35:43 +0700

    refactor code add update fizzbuzz test

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,8 +1,11 @@
 import unittest as ut
 
 def fizzbuzz(number):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
         return "Fizz"
+    
 
 
 #extends class with python
@@ -13,6 +16,9 @@ class TestFizzBuzz(ut.TestCase):
     def test_Input6SholdBeShowFizz(self):
         result = fizzbuzz(6)
         self.assertEqual(result , "Fizz")
+    def test_Input15SholdBeShowFizzBuzz(self):
+        result = fizzbuzz(15)
+        self.assertEqual(result , "FizzBuzz")
 
 ut.main()
 
