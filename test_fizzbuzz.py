de9088eaa9d24e749bcd0f4f3ef147d59e922665
isaman sangbamrung <towlovelove@gmail.com>
2020-07-18 10:44:20 +0700

    rewrite space

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -13,12 +13,15 @@ class TestFizzBuzz(ut.TestCase):
     def test_Input3SholdBeShowFizz(self):
         result = fizzbuzz(3)
         self.assertEqual(result , "Fizz")
+
     def test_Input6SholdBeShowFizz(self):
         result = fizzbuzz(6)
         self.assertEqual(result , "Fizz")
+
     def test_Input15SholdBeShowFizzBuzz(self):
         result = fizzbuzz(15)
         self.assertEqual(result , "FizzBuzz")
+        
     def test_Input450SholdBeShowFizzBuzz(self):
         result = fizzbuzz(450)
         self.assertEqual(result , "FizzBuzz")
