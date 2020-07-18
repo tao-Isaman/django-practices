91b5a6a8eb7a966489a5e99549bb5d5970e29219
isaman sangbamrung <towlovelove@gmail.com>
2020-07-18 14:26:20 +0700

    update test case with image

--- homemaid/maids/models.py
+++ homemaid/maids/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
 class Maid(models.Model):
     name = models.CharField(max_length=300)
+    profile_image = models.FileField()
     birtdate = models.DateField()
     description = models.TextField()
     certificate = models.TextField()
--- homemaid/maids/tests.py
+++ homemaid/maids/tests.py
@@ -1,5 +1,11 @@
 from datetime import date
+from unittest.mock import MagicMock
+import os
+
 from django.test import TestCase
+from django.core.files import File
+
+
 from .models import Maid
 
 
@@ -8,8 +14,8 @@ class TestMaid(TestCase):
     def test_model_should_have_name_defined_fields(self):
         # Given
         Maid.objects.create(
-            name="BB" ,
-            birtdate=date(1998,4,29),
+            name="BB",
+            birtdate=date(1998, 4, 29),
             description="Super mad of the Year",
             certificate="Best Maid 2020",
             salary=3000
@@ -18,12 +24,29 @@ class TestMaid(TestCase):
         # When
         maid = Maid.objects.last()
 
-        #Then
-        self.assertEqual(maid.name , "BB")
-        self.assertEqual(maid.birtdate , date(1998,4,29))
-        self.assertEqual(maid.description , "Super mad of the Year")
-        self.assertEqual(maid.certificate , "Best Maid 2020")
-        self.assertEqual(maid.salary , 3000)
+        # Then
+        self.assertEqual(maid.name, "BB")
+        self.assertEqual(maid.birtdate, date(1998, 4, 29))
+        self.assertEqual(maid.description, "Super mad of the Year")
+        self.assertEqual(maid.certificate, "Best Maid 2020")
+        self.assertEqual(maid.salary, 3000)
 
+        def test_model_should_have_image_defined_fields(self):
+            # Given
+            mock = MagicMock(spec=File)
+            mock.name = "profile.png"
+            Maid.objects.create(
+                name="BB",
+                profile_image=mock.name,
+                birtdate=date(1998, 4, 29),
+                description="Super mad of the Year",
+                certificate="Best Maid 2020",
+                salary=3000
+            )
 
-        
+        # When
+            maid = Maid.objects.last()
+
+        # Then
+            self.assertEqual(maid.profile_image.name, 'profile.png')
+            os.remove('profile.png')
