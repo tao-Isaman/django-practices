b18476901d73fd9068bbc5220ea971e57238b9d2
isaman sangbamrung <towlovelove@gmail.com>
2020-07-19 12:07:21 +0700

    update model to unrequire fields

--- homemaid/maids/models.py
+++ homemaid/maids/models.py
@@ -7,8 +7,8 @@ from django_extensions.db.models import TimeStampedModel
 
 class Maid(TimeStampedModel):
     name = models.CharField(max_length=300)
-    profile_image = models.FileField()
-    birtdate = models.DateField()
-    description = models.TextField()
-    certificate = models.TextField()
-    salary = models.IntegerField()
+    profile_image = models.FileField(blank=True, null=True)
+    birtdate = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    certificate = models.TextField(blank=True, null=True)
+    salary = models.IntegerField(blank=True, null=True)
