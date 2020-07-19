28472c65be880fcd9b28d2e2a706f74c80330afb
isaman sangbamrung <towlovelove@gmail.com>
2020-07-19 10:36:05 +0700

    add templates

--- homemaid/maids/views.py
+++ homemaid/maids/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, request
 from django.views import View
 
 from .models import Maid
@@ -9,10 +9,21 @@ from .models import Maid
 
 class MaidListView(View):
     def get(self, request):
-        html = ""
+        #     html = ""
+        #     for m in Maid.objects.all():
+        #         html += f'<li>{m.name}</li>'
+        #     return HttpResponse(html)
+
+        # ===================================================
+
+        maid_list = []
         for m in Maid.objects.all():
-            html += f'<li>{m.name}</li>'
-        return HttpResponse(html)
+            maid_list.append(m.name)
+        template = 'maidlist.html'
+        context = {
+            'maid_list': maid_list
+        }
+        return render(request, template, context)
 
 
 def maid_another_list_view(request):
